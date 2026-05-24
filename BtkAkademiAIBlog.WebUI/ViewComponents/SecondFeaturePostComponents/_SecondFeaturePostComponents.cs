24c88334b6d2ec0b25728183a6260eac7cdb2565
Bayram Burak Yalav <yalavbayramburak@gmail.com>
2026-05-24 16:01:14 +0300

    Initial commit: Web API and EF Core setup (Removed hardcoded API keys)

---
+++ BtkAkademiAIBlog.WebUI/ViewComponents/SecondFeaturePostComponents/_SecondFeaturePostComponents.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademiAIBlog.WebUI.ViewComponents.SecondFeaturePostComponents
+{
+    public class _SecondFeaturePostComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
