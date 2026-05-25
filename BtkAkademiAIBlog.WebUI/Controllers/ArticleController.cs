72e147e010874f4b77a663109d71df36c7ad17ff
Bayram Burak Yalav <yalavbayramburak@gmail.com>
2026-05-25 17:07:36 +0300

    Refactor Layout and BlogDetail pages using components

---
+++ BtkAkademiAIBlog.WebUI/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademiAIBlog.WebUI.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult ArticleDetail()
+        {
+            return View();
+        }
+
+        public IActionResult ArticleList()
+        {
+            return View();
+        }
+
+        public IActionResult ArticleListByCategory()
+        {
+            return View();
+        }
+    }
+}
--- BtkAkademiAIBlog.WebUI/Program.cs
+++ BtkAkademiAIBlog.WebUI/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}");
+    pattern: "{controller=Article}/{action=ArticleListByCategory}/{id?}");
 
 app.Run();
